44c57967b1d75ef789568bd70664025bb347e5e1
Jose Martinez Saldana <jmartinezsal326@gmail.com>
2022-05-05 19:21:15 -0700

    Modal created

--- frontend/src/components/ImagePage/index.js
+++ frontend/src/components/ImagePage/index.js
@@ -1,11 +1,18 @@
-import React from 'react';
-import {useDispatch} from 'react-redux';
+import React, {useEffect} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
 
+import {getAllImages} from '../../store/images'
 
 
 function ImagePage() {
   const dispatch = useDispatch();
 
+  const imagesObj = useSelector(state => state.images);
+  const images = Object.values(imagesObj);
+
+  useEffect(() =>{
+    dispatch(getAllImages());
+  }, [dispatch])
 
   return(
     <>
